ce7289713bcc5ad65aee704c987369ae62e74369
Jessen Forbush <for21011@byui.edu>
2024-12-01 14:34:16 -0700

    init

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,4 +1,24 @@
 ﻿//Advent of Code Day 1:
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Advent");
 
-//Description:
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
